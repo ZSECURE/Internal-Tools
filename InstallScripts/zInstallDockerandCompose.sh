3f8732f591d94572229004b17b858125a1c4912b
Zak Clifford <46941177+ZSECURE@users.noreply.github.com>
2025-02-26 22:52:02 +0000

    Create zInstallDockerandCompose.sh

---
+++ InstallScripts/zInstallDockerandCompose.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Update package list and install prerequisites
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Docker repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Install Docker
+sudo apt update
+sudo apt install docker-ce -y
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Verify installation
+docker --version
+docker-compose --version
